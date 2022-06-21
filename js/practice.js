cb82d5656188640519d291c6667cb60db24700cd
Artem Shnurko <shnurko.artyom@gmail.com>
2022-06-21 22:36:27 +0300

    js start

---
+++ js/practice.js
@@ -0,0 +1,47 @@
+"use strict";
+// operator if and else
+
+let cost;
+const subscribtion = "premium";
+
+if (subscribtion === "free") {
+  cost = 0;
+} else if (subscribtion === "vip") {
+  cost = 100;
+} else if (subscribtion === "premium") {
+  cost = 1000;
+} else {
+  console.log("Invalid subscription type");
+}
+
+console.log(cost);
+
+// ternal operator
+
+const balance = 1000;
+const message = balance <= 0 ? "positive" : "negative";
+console.log(message);
+
+const age = 18;
+const info = age <= 19 ? "welcome to pornhub" : "have to grow";
+console.log(info);
+
+let price;
+const game = "cs";
+
+// switch instruction
+
+switch (game) {
+  case "dota":
+    price = 100;
+    break;
+
+  case "cs":
+    price = 1800;
+    break;
+
+  default:
+    console.log("invalid have no money");
+}
+
+console.log(price);
