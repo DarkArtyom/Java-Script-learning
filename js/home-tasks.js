c4131d918abca8254be5e53d8e99ff59621de8fc
Artem Shnurko <shnurko.artyom@gmail.com>
2022-07-07 11:02:48 +0200

    task 13

--- js/home-tasks.js
+++ js/home-tasks.js
@@ -433,3 +433,18 @@
 //   return propCount;
 // }
 // countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });
+
+// task 13
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
