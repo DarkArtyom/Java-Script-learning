e322c8a5fe44a44b0771a17ffdb2d8feca861c09
Artem Shnurko <shnurko.artyom@gmail.com>
2022-07-07 22:56:22 +0200

    22

--- js/home-tasks.js
+++ js/home-tasks.js
@@ -470,3 +470,59 @@
 // }
 // getProductPrice("Radar");
 // // вернуть 1300
+
+// task 19
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// // function getAllPropValues(propName) {
+// // Change code below this line
+// const arr = [];
+// const prodKeys = Object.keys(products[0]);
+// console.log(prodKeys);
+// const prodVal = Object.values(products[0]);
+// console.log(prodVal);
+// for (let i = 0; i < prodKeys.length; i += 1) {
+//     if (propName === prodKeys[i]) {
+
+//   }
+// }
+
+//   for (const key of products) {
+//     if (key === propName) {
+//       return arr.push(key.price);
+//     }
+//   }
+//   return arr;
+
+// Change code above this line
+// }
+
+// getAllPropValues("price");
+// Вызов getAllPropValues("price") возвращает [1300, 2700, 400, 1200]
+
+// task 20
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let sum;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return (sum = product.price * product.quantity);
+//     }
+//   }
+//   return 0;
+
+//   // Пиши код выше этой строки
+// }
