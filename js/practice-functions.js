1a2a4e71b15b5ce3ccb71af8fd73c863daec524f
Artem Shnurko <shnurko.artyom@gmail.com>
2022-07-11 19:18:34 +0200

    fn

--- js/practice-functions.js
+++ js/practice-functions.js
@@ -1,4 +1,55 @@
-// loops, for
-for (let a = 10; a <= 22; a += 3) {
-  console.log(a);
-}
+// CALLBACK functions
+
+// const fnA = function (message, callback) {
+//   console.log(message);
+
+//   callback(500);
+// };
+
+// const fnB = function (number) {
+//   console.log(`this is fnB function, number ${number}`);
+// };
+
+// fnA("hi, its me", fnB);
+
+// const mathR = function (a, b, callback) {
+//   const result = callback(a, b);
+
+//   console.log(result);
+// };
+
+// // const add = function (x, y) {
+// //   return x + y;
+// // };
+
+// // const add2 = function (x, y) {
+// //   return x - y;
+// // };
+
+// mathR(2, 33, function (x, y) {
+//   return x + y;
+// });
+// mathR(4, 2, function (x, y) {
+//   return x - y;
+// });
+
+// function createProduct(obj, callback) {
+//   const product = {
+//     id: Math.tandom(),
+//     ...obj
+//   }
+
+//   callback(product)
+// }
+
+// function logProduct(product) {
+//   console.log(product);
+// }
+
+// const p = {
+//   name: "iPhone",
+//   price: 1000,
+//   count: 10
+// }
+
+// createProduct(p, logProduct)
