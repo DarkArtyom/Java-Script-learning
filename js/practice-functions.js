842de6836f163d4c194cc4c7dd3be07f63520729
Artem Shnurko <shnurko.artyom@gmail.com>
2022-07-14 00:08:50 +0200

    Update practice-functions.js

--- js/practice-functions.js
+++ js/practice-functions.js
@@ -137,13 +137,141 @@
 //       filteredNumbers.push(number);
 //     }
 //   });
-
 //   // for (let i = 0; i < numbers.length; i += 1) {
 //   //   if (numbers[i] > value) {
 //   //     filteredNumbers.push(numbers[i]);
 //   //   }
 //   // }
-
 //   // Change code above this line
 //   return filteredNumbers;
 // }
+
+// // task 7
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+//   firstArray.forEach(function newArr(number) {
+//     secondArray.forEach(function arr(number2) {
+//       if (number === number2) {
+//         commonElements.push(number && number2);
+//       }
+//     });
+//   });
+//   return commonElements;
+// Change code above this line
+// }
+
+//   return commonElements;
+//   // Change code above this line
+// }
+// const arr1 = [1, 2, 3];
+// const arr2 = [2, 1, 17, 19];
+// getCommonElements(arr1, arr2);
+
+// task 10
+// const calculateTotalPrice = orderedItems => {
+
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+
+// const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+
+// const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
+// console.log(planetsInUpperCase);
+// console.log(planetsInUpperCase);
+
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Поли", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Киви", score: 94 },
+//   { name: "Хьюстон", score: 64 },
+// ];
+
+// const names = students.map(student => student.name);
+// console.log(names); // ['Манго', 'Поли', 'Аякс', 'Киви', 'Хьюстон']
+
+// task 13
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   const newArr = [];
+//   const copyNumbers = [...numbers];
+//   console.log(copyNumbers);
+//   copyNumbers.forEach((number) => {
+//     console.log(number);
+//     if (number % 2 === 0) {
+//       // console.log(number);
+//       newArr.push(number + value);
+//       // console.log(newArr);
+//     } else {
+//       newArr.push(number);
+//     }
+
+//     return newArr;
+//     // newArr.push((number % 2 === 0)+ value);
+//   });
+//   console.log(newArr);
+//   //   return newArr;
+//   // Change code above this line
+// }
+
+// changeEven([44, 13, 81, 92, 36, 54], 100);
+
+// const changeEvenCopy = changeEven([44, 13, 81, 92, 36, 54], 100);
+// Вызов `changeEven([44, 13, 81, 92, 36, 54], 100)` возвращает новый массив `[144, 13, 81, 192, 136, 154]`
+
+tasks 15
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+// Change code below this line
+
+const titles = books.map((book) => book.title);
+
+tasks 15
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+// Change code below this line
+
+const titles = books.map((book) => book.title);
