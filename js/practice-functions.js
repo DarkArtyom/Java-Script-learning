f6487d32957401ccad6f01e65d3a0b3018d1a812
Artem Shnurko <shnurko.artyom@gmail.com>
2022-07-15 12:46:12 +0200

    add

--- js/practice-functions.js
+++ js/practice-functions.js
@@ -523,3 +523,18 @@
 //   console.log(filteredFriends);
 // };
 // getUsersWithFriend(usersArr);
+
+// task 30 (every)
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+
+// const eachElementInFirstIsEven = firstArray.every((value) => value % 2 === 0);
+// const eachElementInFirstIsOdd = firstArray.every((value) => value % 2 === 1);
+
+// const eachElementInSecondIsEven = secondArray.every((value) => value % 2 === 0);
+// const eachElementInSecondIsOdd = secondArray.every((value) => value % 2 === 1);
+
+// const eachElementInThirdIsEven = thirdArray.every((value) => value % 2 === 0);
+// const eachElementInThirdIsOdd = thirdArray.every((value) => value % 2 === 1);
