926fe24212f9743539137a766257a69dc61a9f3b
Artem Shnurko <shnurko.artyom@gmail.com>
2022-07-28 22:12:22 +0200

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -170,10 +170,131 @@
 //   userName !== "" ? callback(userName) : letMeSeeYourName(callback);
 // };
 
-letMeSeeYourName(greet); // Привет <name>
+// letMeSeeYourName(greet); // Привет <name>
+// const Arr = ["hello", "world", "this", "is", "great"];
+// //   => 'hello world this is great'
+// const smash = (words) => words.join(" ").trim();
+// console.log(smash(Arr));
 
-const Arr = ["hello", "world", "this", "is", "great"];
-//   => 'hello world this is great'
-const smash = (words) => words.join(" ").trim();
+// ------------------------ CLASSES
 
-console.log(smash(Arr));
+// class Blogger {
+//     constructor(settings) {
+//         this.email = settings.email
+//         this.age = settings.age
+//         this.numbersOfPosts = settings.numbersOfPosts
+//         this.topics =settings.topic
+//     }
+//     getInfo() {
+//     return `User ${this.email} is ${this.age} years old and has ${this.numberOfPosts} posts`
+// }
+//     updatePostCount(num) {
+//     this.numberOfPosts = num
+// }
+// }
+// const mango = new Blogger({
+//     email: "mango@mail.com",
+//     age: 24,
+//     numberOfPosts: 20,
+//     topics: ['tech', 'cooking'],
+// });
+// console.log(mango);
+// console.log(mango.getInfo());
+
+// const numbers = 348597;
+
+// function dijitalize(n) {
+//   const str = n.toString();
+//   const arr = [];
+//   const reverse = str.split("").reverse();
+//   // return arr.push(reverse);
+//   console.log(reverse);
+
+//   //   const arr = str(n);
+//   //   console.log(arr);
+
+//   //   const newReverse = arr.reverse(arr);
+
+//   //   console.log(newReverse);
+// }
+
+// function dijitalize(number) {
+//   const str = number.toString();
+//   const arr = str.split("");
+//   const rev = [...arr].reverse();
+//   const newArr = [];
+//   for (let i = 0; i < rev.length; i++) {
+//     return newArr.push(Number(rev[i]));
+//   }
+//   console.log(newArr);
+// }
+
+// dijitalize(348597);
+
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+//  --------------------example 1
+
+// Напиши класс Blogger для создания обьекта блоггера со следующим свойствами:
+
+// email - почта, строка
+// age - возраст, число
+// numberOfPosts - кол-во постов, число
+// topics - массив тем на которых специализируется блоггер
+// Класс ожидает один параметр - объект настроек с одноимёнными свойствами.
+
+// class Blogger {
+//   constructor({ email, age, numberOfPosts = 0, topics = [] }) {
+//     this.email = email;
+//     this.age = age;
+//     this.numberOfPosts = numberOfPosts;
+//     this.topics = topics;
+//   }
+
+//   getInfo() {
+//     return `User ${this.email} is ${this.age} years old and has ${this.numberOfPosts} posts`;
+//   }
+
+//   updatePostCount(number) {
+//     this.numberOfPosts = number;
+//   }
+// }
+
+// const mango = new Blogger({
+//   email: "mango@mail.com",
+//   age: 24,
+//   numberOfPosts: 20,
+//   topics: ["tech", "cooking"],
+// });
+// // Добавь метод getInfo(), который, возвращает строку: User ${почта} is ${возраст} years old and has ${кол-во постов} posts.
+
+// // Добавь метод updatePostCount(value), который в параметре value принимает количество постов которые нужно добавить пользователю.
+
+// console.log(mango);
+// console.log(mango.getInfo());
+// mango.updatePostCount(10);
+// console.log(mango.getInfo());
+// console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 20 posts
+// mango.updatePostCount(5);
+// console.log(mango.getInfo()); // User mango@mail.com is 24 years old and has 25 posts
+
+// const poly = new User({
+//   name: 'poly@mail.com',
+//   age: 19,
+//   numberOfPosts: 17,
+//   topics: ['sports', 'gaming', 'health'],
+// });
+// console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 17 posts
+// poly.updatePostCount(4);
+// console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 21 posts
+
+// var summation = function (num) {
+//   let sum = 0;
+//   for (let i = 0; i <= num; i += 1) {
+//     sum += i;
+//   }
+//   return sum;
+// };
+
+// console.log(summation(8));
